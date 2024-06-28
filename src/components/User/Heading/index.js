62fc8d4d33a3ef84016d28b35c6fac518ec34d8c
Dang Nam <dangnam1303k3@gmail.com>
2024-06-28 14:54:12 +0700

    hoan thien register

--- src/components/User/Heading/index.js
+++ src/components/User/Heading/index.js
@@ -1,47 +1,8 @@
-<<<<<<< HEAD
-import { Link } from 'react-router-dom'
-import './Heading.css'
-import Module from './Module'
-import { handleToggle } from '../handle';
-import { useState } from "react";
-function Heading(){
-    const [isToggle, setIsToggle] = useState(false)
-    return(
-        <div >
-         <nav className="navbar bg-body-tertiary navbar-expand-lg bg-body-tertiary" style={{position:'fixed',zIndex:'1000',width:'100%'}}>
-            <div className="container" >
-                    <div>
-                  
-                            <Link className="navbar-brand" to="/" >
-                            <img src="https://thucpham4.giaodienwebmau.com/wp-content/uploads/2021/10/lg.png" alt="Logo" width="178" height="83" className="d-inline-block align-text-top"/>  
-                            </Link>
-                           
-                    </div>    
-                    <div>
-                         <ul className="nav justify-content-center">
-                        <li className="nav-item">
-                   
-                            <Link className="nav-link text" aria-current="page" to="/">Trang chủ</Link>
-                           
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link  text" href="#">Sản Phẩm</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text" href="#">Tin Tức</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled text" aria-disabled="true">Giới thiệu</a>
-                        </li>
-                        </ul>
-=======
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Module from './Module';
-import { handleToggle } from './Handle';
+import { handleToggle } from '../handle';
 import './Heading.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 function Heading() {
     const [activeLink, setActiveLink] = useState('/');
 
@@ -137,7 +98,6 @@ function Heading() {
                                 <Link to={'/'} className='textlogin'> Đăng nhập/Đăng kí</Link>
                             </button>
                         </div>
->>>>>>> e0ad995efd348471e1d8f6e2db750ff6d8245502
                     </div>
                     <div style={{ marginRight:"30px"}}>
                             <form className="d-flex" role="search">
@@ -160,10 +120,7 @@ function Heading() {
         </div>
     )
 
-<<<<<<< HEAD
-}
-export default Heading  
-=======
 
-export default Heading
->>>>>>> e0ad995efd348471e1d8f6e2db750ff6d8245502
+}
+export default Heading;
+
