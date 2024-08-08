b26b9dcc7a9c715f038e3829251c139008fed649
nguyenanhtuanaaa <anhtuan0578@gmail.com>
2024-08-08 15:29:35 +0700

    giao dien

--- src/components/Admin/home/homemain.js
+++ src/components/Admin/home/homemain.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Pie } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 
 function HomeMain(){
         const data = {
